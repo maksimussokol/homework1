5e38a47d36e90e3a537e52068c496f86a7ae3fc5
maksimussokol <maksimus@2074.ru>
2022-09-17 18:11:58 +0500

    ex1 works

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("enter first nimber");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter second nimber");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("maximum is ");
+    Console.Write(a);
+    Console.WriteLine();
+}
+else
+{
+    Console.Write("maximum is ");
+    Console.Write(b);
+    Console.WriteLine();
+}
