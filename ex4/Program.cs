625c24e50a9b7c9e37d6db667f62f504bc09e916
maksimussokol <maksimus@2074.ru>
2022-09-17 19:22:16 +0500

    ex4 works

---
+++ ex4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("enter the number");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i < n)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+    i = i + 1;
+}
