96dc9893caa39660e99173f333737a717bab3835
maksimussokol <maksimus@2074.ru>
2022-09-17 18:28:07 +0500

    ex3 works

---
+++ ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine("enter first nimber");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter second nimber");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter third nimber");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.Write("maximum is ");
+    Console.Write(a);
+    Console.WriteLine();
+}
+else
+if (b > a && b > c)
+{
+    Console.Write("maximum is ");
+    Console.Write(b);
+    Console.WriteLine();
+}
+else
+{
+    Console.Write("maximum is ");
+    Console.Write(c);
+    Console.WriteLine();
+}
