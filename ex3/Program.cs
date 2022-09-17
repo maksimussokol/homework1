5f9d9ab1e37eb746f25c62b1d70e9f057f966ab0
maksimussokol <maksimus@2074.ru>
2022-09-17 19:09:27 +0500

    ex3 works

---
+++ ex3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("enter number");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+{
+    Console.WriteLine("number is even");
+}
+else
+{
+    Console.WriteLine("number is odd");
+}
